2cbdaf822b09f5d900b3190928ea1a82308ea80f
EgorAndri <55446763+EgorAndri@users.noreply.github.com>
2020-10-03 21:57:05 +0300

    init: add code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(array === undefined || array[0] === undefined){
+    return 0;
+  }
+  else{
+    return Math.min(...array)
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array === undefined || array[0] === undefined){
+    return 0;
+  }
+  else{
+    return Math.max(...array)
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(array === undefined || array[0] === undefined){
+    return 0;
+  }
+  else{
+     let avg = array.reduce((sum, current) => sum + current, 0) / array.length;
+  return avg;
+    }
 }
